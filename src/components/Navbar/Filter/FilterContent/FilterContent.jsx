05ca26da1191836a57383ff1022f939b10886f22
Cristina Ferrer <krishee107@gmail.com>
2023-04-16 19:03:13 +0200

    Listar ciudades en filtercontent

--- src/components/Navbar/Filter/FilterContent/FilterContent.jsx
+++ src/components/Navbar/Filter/FilterContent/FilterContent.jsx
@@ -1,10 +1,25 @@
 import LocationOnIcon from '@mui/icons-material/LocationOn';
+import './FilterContent.css'
+import Stays from '../../../../stays.json'
 
 const FilterContent = () => {
+    const cityList = Stays.map((c) => {
+        return c.city + ", " + c.country;
+    });
+    const cityTotal = [...new Set(cityList)]
+
     return (
         <div className="menuContent">
-            <div className="cityList">
-                <div className="city"><LocationOnIcon /> Helsinky, Finland</div>
+            <div className="cityList" >
+                {cityTotal.map((c) => {
+                    return (
+                        <div className="city">
+                            <LocationOnIcon style={{ fontSize: `20px` }} />
+                            {c}
+                        </div>
+                    )
+                })}
+
             </div>
         </div>
     )
