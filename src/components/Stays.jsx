d45ae2ec8d5617ce81b2ccc72d2f09b4c793020e
Cristina Ferrer <krishee107@gmail.com>
2023-04-16 16:23:44 +0200

    Fix location useState city and staylist div

--- src/components/Stays.jsx
+++ src/components/Stays.jsx
@@ -1,13 +1,21 @@
 import { useState } from "react"
+import House from "./House"
 
 const Stays = () => {
-    const [location, setLocation] = useState('Finland')
+    const [location, setLocation] = useState('Helsinki')
     const [numStays, setNumStays] = useState(12)
 
     return (
-        <div id="stays" style={{ display: `flex`, alignItems: `center`, justifyContent: `space-between`, padding: `60px 0` }}>
-            <h1 style={{ fontWeight: `700`, fontSize: `24px`, lineHeight: `29px`, color: `#333333` }}>Stays in {location}</h1>
-            <div id="numStays">{numStays}+ stays</div>
+        <div id="stays" style={{ padding: `60px 0` }}>
+            <div id="staysHeader" style={{ display: `flex`, alignItems: `center`, justifyContent: `space-between` }}>
+                <h1 style={{ fontWeight: `700`, fontSize: `24px`, lineHeight: `29px`, color: `#333333` }}>Stays in Findland</h1>
+                <div id="numStays">{numStays}+ stays</div>
+            </div>
+
+
+            <div id="staysList">
+                <House />
+            </div>
         </div>
     )
 }
