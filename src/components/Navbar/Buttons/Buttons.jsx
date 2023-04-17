2a84bc794639b08193d5cc5ad85d295ab010a185
Cristina Ferrer <krishee107@gmail.com>
2023-04-17 10:34:46 +0200

    Fix : El filtro se abre al pulsar location o guest pero no el boton de search

--- src/components/Navbar/Buttons/Buttons.jsx
+++ src/components/Navbar/Buttons/Buttons.jsx
@@ -2,11 +2,11 @@ import SearchIcon from '@mui/icons-material/Search';
 
 const Buttons = (props) => {
     return (
-        <div id="menu" className='menu' onClick={props.handleFilterOpen} >
-            <div id="location" className='location' onClick={props.getFilter('location')}>
+        <div id="menu" className='menu' >
+            <div id="location" className='location' onClick={() => { props.getFilter('location'), props.handleFilterOpen() }} >
                 {props.city != 0 ? <div style={{ color: `black` }}>{props.city}</div> : <div > Add location</div>}
             </div>
-            <div id="addGuests" className='addGuests' onClick={props.getFilter('guests')}>
+            <div id="addGuests" className='addGuests' onClick={() => { props.getFilter('guests'), props.handleFilterOpen() }} >
                 {props.guests > 0 ? <div style={{ color: `black` }}>{props.guests} guests</div> : <div> Add guests</div>}
             </div>
             <div id="lupa" className='lupa' onClick={props.search}> <SearchIcon /></div>
