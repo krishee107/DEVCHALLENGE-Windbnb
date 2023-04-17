a1a1ec5ea2b1ca98d652aae5245685357062ce2b
Cristina Ferrer <krishee107@gmail.com>
2023-04-17 11:25:29 +0200

    Funcionamiento del filtro y mostrar resultados

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Navbar from "./components/Navbar/Navbar"
 import Stays from './components/Stays/Stays'
 
 function App() {
-  const [city, setCity] = useState()
-  const [guests, setGuests] = useState()
+  const [city, setCity] = useState(0)
+  const [guests, setGuests] = useState(0)
 
   const setSearch = (city, guests) => {
-    setCity(city)
+    //Arreglamos el formato de city 
+    if (city != 0) {
+      const newCity = city.split(",")
+      setCity(newCity[0])
+    }
+    else setCity(0)
     setGuests(guests)
-
-    console.log(city, guests)
   }
+
+
 
   return (
     <div className="App" style={{ fontFamily: `Montserrat, sans-serif` }}>
       <Navbar search={setSearch} />
-      <Stays />
+      <Stays city={city} guests={guests} />
     </div>
   )
 }
--- src/components/Stays/Stays.jsx
+++ src/components/Stays/Stays.jsx
@@ -1,11 +1,46 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import House from "../House/House"
 import StaysList from '../../stays.json';
 import './Stays.css'
 
-const Stays = () => {
-    const [location, setLocation] = useState('Helsinki')
+const Stays = (props) => {
     const [numStays, setNumStays] = useState(12)
+    const [city, setCity] = useState(0)
+    const [guests, setGuests] = useState(0)
+    const [filterCity, setFilterCity] = useState([])
+
+    useEffect(() => {
+        setCity(props.city);
+        setGuests(props.guests);
+    }, [props.city, props.guests]);
+
+    useEffect(() => {
+        //Si tenemos ciudad y guests
+        if (city != 0 && guests != 0) {
+            const newList = StaysList.filter((house) => {
+                if (house.city == city && house.maxGuests >= guests) return house;
+            })
+            setFilterCity(newList)
+        }
+        //Si tenemos ciudad y no guests
+        else if (city != 0 && guests == 0) {
+            const newList = StaysList.filter((house) => {
+                if (house.city == city) return house;
+            })
+            setFilterCity(newList)
+        }
+        //Si tenemos guests pero no ciudad
+        else if (city == 0 && guests != 0) {
+            const newList = StaysList.filter((house) => {
+                if (house.maxGuests >= guests) return house;
+            })
+            setFilterCity(newList)
+        }
+        //Si no tenemos nada
+        else {
+            setFilterCity(StaysList)
+        }
+    }, [city, guests]);
 
     return (
         <div id="stays" className="stays" >
@@ -16,7 +51,7 @@ const Stays = () => {
 
 
             <div id="staysList" className="staysList">
-                {StaysList.map((h, index) => {
+                {filterCity.map((h, index) => {
                     return (
                         <House key={index} house={h} />
                     );
